e6c87fdc9096d35f163e61dd05d54ee2af4d7bc3
Jai Chaudhary <jai.chaudhary@webileapps.com>
2021-07-13 18:03:24 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
+          alt=""
+        />
+      </Link>
+
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar src="https://avatars.githubusercontent.com/u/59798109?s=400&u=cc4be5ab33f09a6749d0121118b8c6ed4f217dad&v=4" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Components/Search.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./Search.css";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { Button } from "@material-ui/core";
+import PeopleIcon from "@material-ui/icons/People";
+import { useHistory } from "react-router-dom";
+
+function Search() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+  const history = useHistory();
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  return (
+    <div className="search">
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+      <h2>
+        Number of Guests <PeopleIcon />
+      </h2>
+      <input type="number" min={0} defaultValue={2} />
+      <Button onClick={() => history.push("/search")}>Search Airbnb</Button>
+    </div>
+  );
+}
+
+export default Search;
